2bdd8555ab1f80dea1fc9b03896d7f40ad15fc99
syaifulnizamyahya <syaifulnizamyahya@gmail.com>
2024-12-20 17:54:41 +0800

    Added application interfaces Added DTOs

---
+++ ProductApi.Application/DTOs/UpdateProductDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApi.Application.DTOs
+{
+    public class UpdateProductDto
+    {
+        [Required]
+        public string Name { get; set; }
+        [Range(0.01, double.MaxValue)]
+        public decimal Price { get; set; }
+    }
+}
---
+++ ProductApi.Application/Interfaces/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using ProductApi.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApi.Application.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDto>> GetAllProductsAsync();
+        Task<ProductDto> GetProductByIdAsync(Guid id);
+        Task AddProductAsync(CreateProductDto dto);
+        Task UpdateProductAsync(Guid id, UpdateProductDto dto);
+        Task DeleteProductAsync(Guid id);
+    }
+}
