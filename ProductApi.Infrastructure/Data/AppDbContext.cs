8c92513982de45a84aa37344afe6096be41ac6c4
syaifulnizamyahya <syaifulnizamyahya@gmail.com>
2024-12-20 17:59:23 +0800

    Added DbContext Added Repository

---
+++ ProductApi.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApi.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasKey(p => p.Id);
+        }
+    }
+}
