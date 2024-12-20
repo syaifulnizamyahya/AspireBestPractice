5f1b35f9b2562140a86edeee37fb7561eb7bc9e9
syaifulnizamyahya <syaifulnizamyahya@gmail.com>
2024-12-20 17:50:02 +0800

    Added product Uses Domain-driven design

---
+++ ProductApi.Domain/Entities/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApi.Domain.Entities
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; protected set; }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+
+}
---
+++ ProductApi.Domain/Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApi.Domain.Entities
+{
+    public class Product : Entity
+    {
+        public string Name { get; private set; }
+        public decimal Price { get; private set; }
+
+        public Product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public void Update(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
